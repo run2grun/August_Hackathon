ffd4a32f42dfac3b0583e229eaf13b8dd974d1cd
hyedinion <ja03129@naver.com>
2019-07-29 14:50:39 +0900

    처음 셋팅

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\멋사\중앙해커톤\drr\august_hackathon\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
