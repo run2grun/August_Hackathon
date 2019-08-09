4fe169cfcef27ee01e83ad3e7fe38b1d15507a21
hyedinion <ja03129@naver.com>
2019-08-09 23:40:47 +0900

    디비

--- AH_project/here/admin.py
+++ AH_project/here/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Text
+from .models import Text,Movie
 # Register your models here.
 admin.site.register(Text)
+admin.site.register(Movie)
--- AH_project/here/models.py
+++ AH_project/here/models.py
@@ -8,3 +8,10 @@ class Text(models.Model):
 
     def __str__(self):
         return self.text
+
+class Movie(models.Model):
+    title=models.CharField(max_length=50,null=True)
+    text=models.TextField()
+
+    def __str__(self):
+        return self.title
