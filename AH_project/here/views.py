f7b14f7781a1f7d3f16fb4b7bcb42599dc4d52ba
book3620 <book3620@naver.com>
2019-08-09 23:55:38 +0900

    ddd

--- AH_project/here/views.py
+++ AH_project/here/views.py
@@ -55,7 +55,7 @@ def parse(request):
     if check == 0:
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
-        driver = webdriver.Chrome("C:\\Users\\user\\Downloads\\chromedriver_win32\\chromedriver.exe")
+        driver = webdriver.Chrome("C:\\Users\\choi\\Downloads\\chromedriver_win32\\chromedriver.exe")
         # ,chrome_options=options -> 창안보이게 하기
         # 혜진 경로 C:\\Users\\user\\Downloads\\chromedriver_win32\\chromedriver.exe
         print('크롤링 시작')
